5195f265de8bfd549b81c34a7851d11d5395970c
Dylan Sather <dylan.sather@gmail.com>
2018-06-13 22:55:12 -0700

    Adding tests

--- 05-slice-sequences/README.md
+++ 05-slice-sequences/README.md
@@ -1,2 +1,3 @@
 * We can slice any class that implements a `__getitem__` and `__setitem__` method.
-* 
+* Slices return a new list. Modifying this new slice will not affect the original list, since it's a copy.
+* When using slices in assignment, it will affect the original list (see `tests.py`).
--- 05-slice-sequences/tests.py
+++ 05-slice-sequences/tests.py
@@ -6,3 +6,26 @@ l = ['a', 'b', 'c', 'd', 'e']
 
 def test_standard_slice():
     assert l[0:3] == ['a', 'b', 'c']
+
+
+def test_remove_zero_index():
+    """ But we should remove the initial 0 to remove noise
+    """
+    assert l[:3] == ['a', 'b', 'c']
+
+
+def test_remove_end_index():
+    """ We should also remove the end index if we slice to the
+        end of the list, also to reduce noise
+    """
+    assert l[3:] == ['d', 'e']
+
+
+def test_negative_slice():
+    assert l[-3:-1] == ['c', 'd']
+
+
+def test_slice_assigment():
+    a = [1, 2, 3]
+    a[0:2] = [5, 6]
+    assert a == [5, 6, 3]
