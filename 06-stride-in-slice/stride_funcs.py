d210993acfd66016c9c72f120a0fe88a45b7683a
Dylan Sather <dylan.sather@gmail.com>
2018-06-13 23:07:46 -0700

    Adding functions to get even and odd elements

--- 06-stride-in-slice/stride_funcs.py
+++ 06-stride-in-slice/stride_funcs.py
@@ -1,2 +1,10 @@
 def reverse_list(li):
     return li[::-1]
+
+
+def get_elements_at_even_indices(li):
+    return li[1::2]
+
+
+def get_elements_at_odd_indices(li):
+    return li[::2]
--- 06-stride-in-slice/tests.py
+++ 06-stride-in-slice/tests.py
@@ -3,3 +3,11 @@ import stride_funcs
 
 def test_reverse_list():
     assert stride_funcs.reverse_list([1, 2, 3]) == [3, 2, 1]
+
+
+def test_odd_elements():
+    assert stride_funcs.get_elements_at_odd_indices([1, 2, 3]) == [1, 3]
+
+
+def test_even_elements():
+    assert stride_funcs.get_elements_at_even_indices([1, 2, 3]) == [2]
